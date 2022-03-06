8520eb3559c14bb7f99152a5882080b382313b95
SkymeFactor <sergei.51351@yandex.ru>
2022-03-07 01:47:32 +0300

    basic program logic added

--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,119 @@
 #include <iostream>
 #include <cmath>
-#include <chrono>
-#include <iomanip>
 #include <fstream>
+#include <limits>
 #include <omp.h>
+
+#include "omp_estimator.h"
+
+
+
+void report_failure(std::string_view msg) {
+    std::cerr << "[ ERROR ]: " << msg << '\n';
+    std::exit(EXIT_FAILURE);
+}
 
 
 double f_x(double x) {
     return std::log(std::sin(x));
 }
 
-int execution_time = -1;
 
-template<class T>
-double get_integral(T func, float a, float b, double err_val) {
+double get_integral(double (*func)(double), double left, double right, double err_val) {
     double area = 0.0;
-    double step = b - a;
-    double area_prev = 1.0;
+    double step = right - left;
+    double area_prev = std::numeric_limits<double>::max();
     
-    auto t1 = std::chrono::high_resolution_clock::now();
-    while( (area_prev - area) / 3 >= err_val ) {
+    omp_estimator::timer_begin();
+    while( std::abs(area - area_prev) / 3 >= err_val ) {
         area_prev = area;
         area = 0.0;
         step = step / 2;
-        #pragma omp parallel shared(a, b, step, area)
-        {
-            #pragma omp for reduction(+: area) // schedule(runtime)
-            for (int i = 0; i < (b - a) / step; i++) {
-                area += func(a + step / 2 + i * step) * step;
-            }
-            /*#pragma omp single
-            std::cout << "Step = " << step << "; Area: " << area << '\n';*/
+
+        #pragma omp parallel for reduction(+: area) schedule(runtime)
+        for (int i = 0; i < (right - left) / step; i++) {
+            area += func(left + step / 2 + i * step) * step;
         }
     }
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-    
-    /*std::cout << "Cycles: " << (int)((b - a) / step) << '\n';
-    std::cout << "Duration: " << duration.count() << " ms\n";*/
-    execution_time = duration.count();
+    omp_estimator::timer_end();
     
     return area;
 }
 
-template<class T>
-double get_integral_no_omp(T func, float a, float b, double err_val) {
+
+double get_integral_no_omp(double (*func)(double), double left, double right, double err_val) {
     double area = 0.0;
-    double step = b - a;
-    double area_prev = 1.0;
+    double step = right - left;
+    double area_prev = std::numeric_limits<double>::max();
     
-    auto t1 = std::chrono::high_resolution_clock::now();
-    while( (area_prev - area) / 3 >= err_val ) {
+    omp_estimator::timer_begin();
+    while( std::abs(area - area_prev) / 3 >= err_val ) {
         area_prev = area;
         area = 0.0;
         step = step / 2;
-        for (int i = 0; i < (b - a) / step; i++) {
-            area += func(a + step / 2 + i * step) * step;
+        
+        for (int i = 0; i < (right - left) / step; i++) {
+            area += func(left + step / 2 + i * step) * step;
         }
     }
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
-    execution_time = duration.count();
+    omp_estimator::timer_end();
     
     return area;
 }
+
 
 int main(int argc, char* argv[]) {
-    /// TODO: work with files
-    if (argc == 4) {
-        std::string in_filename {argv[1]};
-        std::string out_filename {argv[2]};
-        //std::cout << in_filename << '\n';
-        //std::cout << out_filename << '\n';
-        omp_set_num_threads(std::atoi(argv[3]));
-    } else {
-        /// TODO: logging function
-        std::cerr << "[ ERROR ] Invalid number of arguments\n";
-        std::exit(1);
-    }
+    
+    if (argc != 4)
+        ::report_failure("Invalid number of arguments\n");
+    
+    double left_bound, right_bound, error_rate;
+    bool omp_enable_flag = true;
 
-    double avg_time = 0;
-    //std::cout << std::fixed << std::setprecision(10);
-    for (int i = 0; i < 100; i++) {
-        get_integral_no_omp(f_x, 0, M_PI, 0.00001);
-        avg_time += execution_time;
+    std::ifstream fin(argv[1]);
+    std::ofstream fout(argv[2]);
+    int thr_num = std::atoi(argv[3]);
+    
+    if (!fin.is_open() || !fout.is_open())
+        ::report_failure("Unable to open file");
+    
+    if (thr_num < -1)
+        ::report_failure("Invalid number of threads\n");
+    
+    switch (thr_num) {
+        case -1: omp_enable_flag = false; break;
+        case 0: break;
+        default: omp_set_num_threads(thr_num); break;
     }
-    //std::cout << get_integral(f_x, 0, M_PI, 0.00001 ) << '\n';
-    printf("Time (%i thread(s)): %g ms\n", std::atoi(argv[3]), avg_time / 100);
+    
+    omp_estimator::PerformanceEstimator est;
+    auto func = omp_enable_flag ? get_integral : get_integral_no_omp;
+
+    while (!fin.eof()) {
+        fin >> left_bound >> right_bound >> error_rate >> std::ws;
+        
+        if (fin.fail()) {
+            fin.close();
+            fout.close();
+            ::report_failure("Unable to read from file");
+        }
+
+        if (error_rate <= 0) {
+            fin.close();
+            fout.close();
+            ::report_failure("error_rate is out of range");
+        }
+
+        est.estimate(func, &f_x, left_bound, right_bound, error_rate);
+
+        std::cout << "Time (" << thr_num << " thread(s)): "
+                  << est.get_elapsed_time() << " ms\n";
+        
+        fout << std::any_cast<double>(est.get_return_value()) << '\n';
+    };
+    
+    fin.close();
+    fout.close();
     
     return 0;
 }
