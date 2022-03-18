2829b4eaf393e8f4945abbc14123078ef51ed355
SkymeFactor <sergei.51351@yandex.ru>
2022-03-19 01:32:05 +0300

    Parallel algorithm improved

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <omp.h>
+#include <memory.h>
 
 #include "omp_estimator.h"
 #include "P5_Image.h"
@@ -8,24 +9,55 @@
 
 Image_Hist compute_histogram(P5_Image img) {
     Image_Hist hist_result(img.max_val + 1, 0);
-    
-    omp_estimator::timer_begin();
+
+    omp_estimator::timer_begin();   // Start of the time measurement
+
+    uint8_t* data = img.data.data();
+    auto len = img.max_val + 1;
+    std::size_t remainder = img.data.size() % 4;
+    std::uint32_t hist[len * omp_get_max_threads()];
+    memset(hist, 0, sizeof(std::uint32_t) * len * omp_get_max_threads());
+
     #pragma omp parallel
     {
-        Image_Hist _hist_private(img.max_val + 1, 0);
+        unsigned thr_offset = static_cast<unsigned>(len * omp_get_thread_num());
 
         #pragma omp for schedule(runtime)
-        for (size_t i = 0; i < img.data.size(); ++i) {
-            _hist_private[img.data[i]]++;
-        }
-
-        #pragma omp critical
-        {
-            for (auto i = 0; i < hist_result.size(); ++i)
-                hist_result[i] += _hist_private[i];
+        for (std::size_t i = 0; i < img.data.size() - remainder; i += 4) {
+            ++hist[thr_offset + static_cast<unsigned>(data[i])];
+            ++hist[thr_offset + static_cast<unsigned>(data[i + 1])];
+            ++hist[thr_offset + static_cast<unsigned>(data[i + 2])];
+            ++hist[thr_offset + static_cast<unsigned>(data[i + 3])];
         }
     }
-    omp_estimator::timer_end();
+
+    switch (remainder) {
+        case 3: ++hist[data[img.data.size() - 3]];
+        case 2: ++hist[data[img.data.size() - 2]];
+        case 1: ++hist[data[img.data.size() - 1]];
+        case 0: break;
+    }
+    
+    remainder = hist_result.size() % 4;
+    for (int thr = 0; thr < omp_get_max_threads(); ++thr) {
+        std::size_t thr_offset = thr * len;
+        
+        for (std::size_t i = 0; i < hist_result.size() - remainder; i += 4) {
+            hist_result[i] += hist[thr_offset + i];
+            hist_result[i + 1] += hist[thr_offset + i + 1];
+            hist_result[i + 2] += hist[thr_offset + i + 2];
+            hist_result[i + 3] += hist[thr_offset + i + 3];
+        }
+        
+        switch (remainder) {
+            case 3: hist_result[hist_result.size() - 3] += hist[thr_offset + hist_result.size() - 3];
+            case 2: hist_result[hist_result.size() - 2] += hist[thr_offset + hist_result.size() - 2];
+            case 1: hist_result[hist_result.size() - 1] += hist[thr_offset + hist_result.size() - 1];
+            case 0: break;
+        }
+    }
+
+    omp_estimator::timer_end();     // End of the time measurement
 
     return hist_result;
 }
