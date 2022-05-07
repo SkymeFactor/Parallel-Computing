b0924fecad1f5c45bb00571c232f491f021be67c
SkymeFactor <sergei.51351@yandex.ru>
2022-05-07 08:45:32 +0300

    main added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,89 @@
 #include <iostream>
+#include <exception>
+#include <cstdlib>
+#include <iomanip>
+
+#include "ezocl_core.h"
+#include "utilities.h"
+
 
 
 int main(int argc, char* argv[]) {
+    if (argc < 5)
+        return EXIT_FAILURE;
+    
+    int ocl_device_number {std::atoi(argv[1])};
+    std::string in_filename {argv[2]};
+    std::string out_filename {argv[3]};
+    int algorithm_implementation {std::atoi(argv[4])};
+
+    std::string ocl_build_options {"-cl-std=CL1.2 -DBLOCK_SIZE=" + std::to_string(LOCAL_SIZE)};
+    std::string kernel_filename {"src/kernels/floatMatMul.cl"};
+    std::string kernel_name {""};
+    std::vector<std::size_t> local_worksize {LOCAL_SIZE, LOCAL_SIZE};
+
+    switch (algorithm_implementation) {
+        case 1: kernel_name = "matMulSimple"; break;
+        case 2: kernel_name = "matMulBlocked"; break;
+        case 3:
+            kernel_name = "matMulBlockedVectorized";
+            local_worksize = {LOCAL_SIZE, LOCAL_SIZE / 8};
+        break;
+        default: std::cerr << "[ Error ]: Invalid algorithm version\n"; return EXIT_FAILURE;
+    }
+
+    auto [mat_in1, mat_in2] = generateTestData(2000, 1000, 2000); //parse_matrices_file(in_filename);
+    decltype(mat_in1) mat_out;
+    mat_out.setSize(mat_in1.getHeight(), mat_in2.getWidth());
+
+    Matrix<float> mat_eval = mat_mul_cpu(mat_in1, mat_in2);
+    
+    try {
+        auto ocl_devices = ezocl::DeviceManager::getDevices();
+        if (ocl_devices.size() == 0 || ocl_device_number > ocl_devices.size() - 1)
+            return EXIT_FAILURE;
+        
+        cl_uint hA = mat_in1.getHeight();
+        cl_uint wA = mat_in1.getWidth();
+        cl_uint wB = mat_in2.getWidth();
+        ezocl::Buffer heightA{&hA, sizeof(hA), ezocl::BufferType::IN_SCALAR};
+        ezocl::Buffer widthA {&wA, sizeof(wA), ezocl::BufferType::IN_SCALAR};
+        ezocl::Buffer widthB {&wB, sizeof(wB), ezocl::BufferType::IN_SCALAR};
+        ezocl::Buffer matA {&mat_in1[0][0], mat_in1.getSizeInBytes(), ezocl::BufferType::IN_BUFFER};
+        ezocl::Buffer matB {&mat_in2[0][0], mat_in2.getSizeInBytes(), ezocl::BufferType::IN_BUFFER};
+        ezocl::Buffer matC {&mat_out[0][0], mat_out.getSizeInBytes(), ezocl::BufferType::OUT_BUFFER};
+
+        ezocl::Kernel kernel {
+            kernel_name,
+            ezocl::makeGlobalNDRange(local_worksize, mat_in2.getWidth(), mat_in1.getHeight()),
+            local_worksize,
+            heightA, widthA, widthB,
+            matA, matB, matC
+        };
+
+        ezocl::Program my_program(kernel_filename, ocl_devices[ocl_device_number], kernel);
+        my_program.execute(ocl_build_options);
+        
+        /// TEST: Compare CPU and GPU outputs
+        if (compareMatrices(mat_eval, mat_out)) {
+            std::cout << "\n[ TEST ] CPU and GPU results equal: PASSED\n";
+        } else {
+            std::cout << "\n[ TEST ] CPU and GPU results equal: FAILED\n";
+        }
+
+        auto total_time = my_program.getTotalKernelTime();
+        auto kernel_time = my_program.getKernelExecutionTime();
+
+        std::cout << std::showpoint \
+                << "\nTime: " << total_time[0] / 1000000.0 << '\t' \
+                << kernel_time[0] / 1000000.0 << std::noshowpoint << " \n";
+
+        save_matrix_to_file(mat_out, out_filename);
+
+    } catch(const std::exception& e) {
+        std::cerr << "[ Error ]: " << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
 
     return 0;
 }
